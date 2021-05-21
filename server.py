27aba0ab116f90f7d9152f8f2926c1c5ef154fb0
Andrei Coman <andrei.coman@3pillarglobal.com>
2021-05-21 10:56:14 +0300

    Enable simple gRPC server

--- server.py
+++ server.py
@@ -1,1 +1,22 @@
-print("Hello World")
+from concurrent import futures
+import logging
+import time
+
+import grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    while True:
+        try:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+        except KeyboardInterrupt:
+            server.stop(0)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
